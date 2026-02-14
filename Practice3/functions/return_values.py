0c17fbca44773296c06025321d2ea05c625073a4
RedWodoo <alikakenov567@gmail.com>
2026-02-14 15:52:22 +0500

    Practice 3

---
+++ Practice3/functions/return_values.py
@@ -0,0 +1,28 @@
+'''def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
+
+def get_greeting():
+  return "Hello from a function"
+
+print(get_greeting())
+
+
+def my_function(x, y):
+  return x + y
+
+result = my_function(5, 3)
+print(result)
+
+
+def my_function():
+  return ["apple", "banana", "cherry"]
+
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+'''
---
+++ Practice3/inheritance/method_overriding.py
@@ -0,0 +1,4 @@
+'''class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+    '''
